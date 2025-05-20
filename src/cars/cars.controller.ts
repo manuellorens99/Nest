5a09cfea8e9fdc8ac3ee92a152cff7820df63ec2
Manuel Llorens <pixelartrecompilation@gmail.com>
2025-05-20 08:17:32 +0200

    Car .service Added

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,17 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-    private cars = ['Toyota','Honda','Mazda'];
+    constructor(
+        private readonly carsService: CarsService
+    ){}
     @Get()
     getAllCars(){
-        return this.cars;
+        return this.carsService.findAll()
     }
-    @Get(':id')
-    getCarById(@Param( 'id' ) id ){
-        console.log({id})
-        //return {id}
-        return this.cars[id];
+     @Get(':id')
+     getCarById(@Param( 'id' ) id: string ){
+         console.log({id});
+         return this.carsService.getCarById(+id);
         //Al colocar en postman: http://localhost:3000/cars/3
         //me devolverá el carro que está en la posición '#3'
         //de mi lista. Si mi lista no tiene '#3', debemos crear
