3c6ad51265f4e6e3e709d0e0112432e50b9d7ccc
Manuel Llorens <pixelartrecompilation@gmail.com>
2025-05-20 09:28:26 +0200

    All controllers explained.

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -19,5 +19,23 @@ export class CarsController {
         //de mi lista. Si mi lista no tiene '#3', debemos crear
         //un método de seguridad para que no existan esos cabos
         //sueltos en nuestro proyecto.
+    }
+    @Post()
+    createCar(@Body() body: any){
+        return body;
+    }
+    @Patch('/:id')
+    updateCar(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() body: any)
+        {
+        return body;
+        }
+    @Delete('/:id')
+    deleteCar(@Param('id', ParseIntPipe) id: number){
+        return {
+            method: 'delete',
+            id
+        };
     }
 }
